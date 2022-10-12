1245587877880da1d186fb635917f1e52f8338ea
Рустам <rustam19871203@gmail.com>
2022-10-12 13:58:47 +0400

    Циклы часть 1. Домашка 1 с 1-4 задачу.

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//Задача 1
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++)
+            System.out.println("Итерация №" + i);
+
+        //Задача 2
+
+        for (int i = 10; i > 0; i--)
+            System.out.println("Итерация №" + i);
+
+        //Задача 3
+
+        for (int i = 0; i <= 17 ; i = i + 2)
+            System.out.println("Итерация №" + i);
+
+        //Задача 4
+        for (int i = 10; i >= -10 ; i --)
+            System.out.println("Итерация №" + i);
+    }
+
+
+
+}
